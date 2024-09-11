09b2c2e0f1142e6dc1bfc4d485e5e8eadb40dee8
Felix <felixstangl3@gmail.com>
2024-09-11 16:19:11 +0200

    vault backup: 2024-09-11 16:19:11
    Affected files:
    scripts/english_vocab.py

--- scripts/english_vocab.py
+++ scripts/english_vocab.py
@@ -1,7 +1,7 @@
 import requests
 
 api_root = "https://linguee-api.fly.dev/api/v2"
-resp = requests.get(f"{api_root}/translations", params={"query": "stern", "src": "de", "dst": "en"})
+resp = requests.get(f"{api_root}/translations", params={"query": "house", "src": "en", "dst": "de"})
 for lemma in resp.json():
     for translation in lemma['translations']:
         print(f"{lemma['text']} -> {translation['text']}")
