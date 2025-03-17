b0751beebf285970fafe4b9b0494bafde5ec5425
Felix Stangl <felixstangl@mail.de>
2025-03-17 21:36:09 +0100

    -> 2025-03-17 21:36:09 -> Backup
    Affected files:
    scripts/vocabulary.py

--- scripts/vocabulary.py
+++ scripts/vocabulary.py
@@ -17,7 +17,7 @@ closure_end = "}}"
 
 output_with_examples = (True, False)
 separator = ":::"
-amount_of_words_per_execution = 5
+amount_of_words_per_execution = 21
 
 # ================================
 
